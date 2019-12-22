0452c7659b26ad3c4cd5bc4449fcfa6f2eb7f255
Sandeep Kundala <sandeepkundala@gmail.com>
2019-12-22 01:57:44 -0500

    Update analysis_unit_test.py

--- Code/analysis_unit_test.py
+++ Code/analysis_unit_test.py
@@ -1,84 +1,93 @@
+""" USECASE 2 TESTING """
 import unittest
 import json
 import analysis
 
 with open("/home/CSC510-23/Code/data.json") as json_file:
-    data = json.load(json_file)
-libraries = data["libraries"]
-columnNames = data["columnNames"]
-wrngColEx = data["wrongTargetColumnException"]
+    DATA = json.load(json_file)
+LIBRARIES = DATA["libraries"]
+COLUMN_NAMES = DATA["columnNames"]
+WRONG_COL_EX = DATA["wrongTargetColumnException"]
+PARA_COUNT_CAT = 5
+PARA_COUNT_NUM = 4
 
-parameter_count_categorical = 5
-parameter_count_numerical = 4
 
-# Checks the lines in the file if the dataset given by user is categorical
-def contentCheck_categorical():
+def contentcheck_categorical():
+    """ Checks the lines in the file if the dataset given by user is categorical """
     filename = "Analysis.txt"
-    a = ""
+    temp_line = ""
     count = 0
     for line in open(filename, 'r'):
-        a = a + line
-    if "VALUE COUNTS" in a:
+        temp_line = temp_line + line
+    if "VALUE COUNTS" in temp_line:
         count = count + 1
-    if "DATA INFORMATION" in a:
+    if "DATA INFORMATION" in temp_line:
         count = count + 1
-    if "MEAN, MEDIAN AND MODE:" in a:
+    if "MEAN, MEDIAN AND MODE:" in temp_line:
         count = count + 1
-    if "Correlation" in a:
+    if "Correlation" in temp_line:
         count = count + 1
-    if "Normality Tests" in a:
+    if "Normality Tests" in temp_line:
         count = count + 1
-    return count 
+    return count
 
-# Checks the lines in the file if the dataset given by user is numerical
-def contentCheck_numerical():
+
+def contentcheck_numerical():
+    """ Checks the lines in the file if the dataset given by user is numerical """
     filename = "Analysis.txt"
-    a = ""
+    temp_line = ""
     count = 0
     for line in open(filename, 'r'):
-        a = a + line
-    if "DATA INFORMATION" in a:
+        temp_line = temp_line + line
+    if "DATA INFORMATION" in temp_line:
         count = count + 1
-    if "MEAN, MEDIAN AND MODE:" in a:
+    if "MEAN, MEDIAN AND MODE:" in temp_line:
         count = count + 1
-    if "Correlation" in a:
+    if "Correlation" in temp_line:
         count = count + 1
-    if "Normality Tests" in a:
+    if "Normality Tests" in temp_line:
         count = count + 1
-    return count 
-    
-    
+    return count
+
 
 class TestyAnalysisMethods(unittest.TestCase):
+    """ TEST CASES """
     # Use case 2 happy flow - to check the contents of the file produced after performing the EDA
     def test_analysis_categorical(self):
-            analysis.analysisInteraction("Datasets/Crime1.csv","Category")
-            count_cat = contentCheck_categorical()
-            self.assertEquals(count_cat,parameter_count_categorical)
-            
+        """ Check if categorical """
+        analysis.analysis_interaction("Datasets/Crime1.csv", "Category")
+        count_cat = contentcheck_categorical()
+        self.assertEqual(count_cat, PARA_COUNT_CAT)
+
     def test_analysis_numerical(self):
-            analysis.analysisInteraction("Datasets/Wine.csv","Class")
-            count_cat = contentCheck_numerical()
-            self.assertEquals(count_cat,parameter_count_numerical)
-            
-    # usecase 2 - alternate flow - if the wrong target column is provided 
+        """ Check if numerical """
+        analysis.analysis_interaction("Datasets/Wine.csv", "Class")
+        count_cat = contentcheck_numerical()
+        self.assertEqual(count_cat, PARA_COUNT_NUM)
+
+    # usecase 2 - alternate flow - if the wrong target column is provided
     def test_analysis_target_cat(self):
-           self.assertEqual(analysis.analysisInteraction("Datasets/Crime1.csv", "Hululu"), wrngColEx)
-    
+        """ Test the wrong column given scenario """
+        self.assertEqual(analysis.analysis_interaction("Datasets/Crime1.csv", "test"), WRONG_COL_EX)
+
     def test_analysis_target_num(self):
-           self.assertEqual(analysis.analysisInteraction("Datasets/Wine.csv", "Hululu"), wrngColEx) 
-    
-    # Checks if the result of execution of the call to Use case 2 is not empty 
+        """ Test the wrong column given scenario """
+        self.assertEqual(analysis.analysis_interaction("Datasets/Wine.csv", "test"), WRONG_COL_EX)
+
+    # Checks if the result of execution of the call to Use case 2 is not empty
     def test_analysis_result_cat(self):
-        self.assertIsNotNone(analysis.analysisInteraction("Datasets/Crime1.csv","Category"))
-        
+        """ Test the categorical data set """
+        self.assertIsNotNone(analysis.analysis_interaction("Datasets/Crime1.csv", "Category"))
+
     def test_analysis_result_num(self):
-        self.assertIsNotNone(analysis.analysisInteraction("Datasets/Wine.csv","Class"))
-    
-    # Checks if the file has been created after executing use case 2 
-    def test_fileCreation(self):
-        self.assertIsNotNone(open("Analysis.txt","r"))
-        
+        """ Test the numerical data set """
+        self.assertIsNotNone(analysis.analysis_interaction("Datasets/Wine.csv", "Class"))
+
+    # Checks if the file has been created after executing use case 2
+    def test_file_creation(self):
+        """ Test file creation"""
+        self.assertIsNotNone(open("Analysis.txt", "r"))
+
+
 if __name__ == '__main__':
     unittest.main()
-        
